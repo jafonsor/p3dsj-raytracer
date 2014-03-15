a0cb7c474cca8ee2b70b8bb3cc9f8424fa3d845b
Joao Rodrigues <jrodriguesnum1@hotmail.com>
2014-03-15 18:42:55 +0000

    Implement interseciton checking for spheres

--- src/scene/Sphere.cpp
+++ src/scene/Sphere.cpp
@@ -7,5 +7,38 @@ Sphere::Sphere(float x, float y, float z, float r)
 }
 
 Intersection * Sphere::checkIntersection(Ray * ray) {
+	float a = glm::dot(ray.direction,ray.direction);
+	float b = 2 * glm::dot(ray.direction, ray.origin);
+	float c = dot(ray.origin, ray.origin) - (_radius * _radius);
+
+	float discriminant = b * b - 4 * a * c;
+	if(discriminant < 0) {
+		return nullptr; // there is no intersection
+	}
+
+	float t0 = (-b - glm::sqrt(b * b - 4 * a * c)) / (2 * a);
+	float t1 = (-b + glm::sqrt(b * b - 4 * a * c)) / (2 * a);
+	float t;
+
+	if(t0 < 0 && t1 < 0) {
+		return nullptr; // both roots are negative
+	}
+
+	if(t0 > t1) {
+		t = t1
+	} else {
+		t = t0;
+	}
+
+	if(t == 0) {
+		return nullptr; // the intersection is on the eye
+	}
+
+	Intersection * result = new Intersection();
+	result->position      = ray->f(t);
+	result->normal        = result->position - _center;
+	result->object        = this;
+	result->distanceToEye = t;
 	
+	return result;
 }
