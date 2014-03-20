1a843c10ebcce0e73554c96f2a9b8dc2e16fe30f
João Rodrigues <jrodriguesnum1@hotmail.com>
2014-03-20 00:10:18 +0000

    Add constructors to light.

--- src/scene/Light.h
+++ src/scene/Light.h
@@ -3,9 +3,24 @@
 
 #include<glm.hpp>
 
+
+const glm::vec3 DEFAULT_POSITION = glm::vec3(0,0,0);
+// default light color is white
+const glm::vec3 DEFAULT_COLOR_LIGHT = glm::vec3(1,1,1);
+
 typedef struct light {
 	glm::vec3 position;
 	glm::vec3 color;
+
+	light()
+			: position(DEFAULT_POSITION), color(DEFAULT_COLOR_LIGHT) { /* empty */ }
+
+	light(glm::vec3 position)
+		: position(position), color(DEFAULT_COLOR_LIGHT) { /* empty */ }
+
+	light(glm::vec3 position, glm::vec3 color)
+		: position(position), color(color) { /* empty */ }
+
 } Light;
 
 Light *  createLight(glm::vec3 position, glm::vec3 color);
