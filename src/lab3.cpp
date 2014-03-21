e54f076ad80a4a8ca251c2b5764baa4129d6b6bd
pedroth <hiperpc@yahoo.com>
2014-03-21 17:39:18 +0000

    added more spheres to the scene

--- src/lab3.cpp
+++ src/lab3.cpp
@@ -14,7 +14,7 @@ void test(Scene * s) {
 	s->addObject(new Object(glm::vec3(0.0f, 0.0f, 1.0f), 1.0f, 1.0f, 10.0f,new Sphere(1.0f, 0.5f, 1.0f, 1.0f)));
 	s->addObject(new Object(glm::vec3(1.0f, 0.0f, 0.0f), 1.0f, 1.0f, 10.0f, new Sphere(2.0f, 0.7f, 1.5f, 0.1f)));
 	s->addLight(createLight(glm::vec3(10.0f), glm::vec3(1.0f)));
-	s->addLight(createLight(glm::vec3(4.0f,0.0f,5.0f), glm::vec3(1.0f)));
+	//s->addLight(createLight(glm::vec3(4.0f,0.0f,5.0f), glm::vec3(1.0f)));
 	s->setCamera(new Camera(glm::vec3(4.0f,0.0f,0.0f), glm::vec3(1.5f,0.0f,0.0f), glm::vec3(0.0f, 0.0f, 1.0f), PI / 2, resx, resy));
 	ray.point = glm::vec3(2.0f,0.0f,0.0f);
 	ray.direction = glm::normalize(glm::vec3(0.0f, 1.2f, 0.0f) - ray.point);
