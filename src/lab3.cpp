248ca243e62d54b76b9b7c8554c26e607a482184
João Rodrigues <jrodriguesnum1@hotmail.com>
2014-04-08 02:16:15 +0100

    Added multithreading to DrawApi.

--- src/lab3.cpp
+++ src/lab3.cpp
@@ -54,7 +54,7 @@ void testTriangle() {
 }
 
 int main(int argc, char *argv[]) {
-	const char * fileName = "resources/balls_medium.nff";
+	const char * fileName = "resources/balls_low.nff";
 	// read the file name from the argument if any given
 	if(argc >= 2) {
 		fileName = argv[1];
