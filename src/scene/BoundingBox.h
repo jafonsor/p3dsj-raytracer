63097e43c3079f46e7964216a9760830bd079da3
João Rodrigues <jrodriguesnum1@hotmail.com>
2014-04-14 22:29:39 +0100

    Almost finished the the tmax and tdelta

--- src/scene/BoundingBox.h
+++ src/scene/BoundingBox.h
@@ -2,10 +2,19 @@
 #define BOUNDING_BOX_H
 
 #include "Geometry.h"
+#include "Plane.h"
 
 class BoundingBox : Geometry {
 	glm::vec3 _minCorner;
 	glm::vec3 _maxCorner;
+
+	Plane negPlaneX();
+	Plane posPlaneX();
+	Plane negPlaneY();
+	Plane posPlaneY();
+	Plane negPlaneZ();
+	Plane posPlaneZ();
+
 public:
 	BoundingBox();
 	void setDimentions(glm::vec3 &minCorner, glm::vec3 &maxCorner);
@@ -14,6 +23,7 @@ public:
 	// Update if necessary the min and max corners to contain the 'corner'
 	void updateMaxCorner(glm::vec3 &corner);
 	void updateMinCorner(glm::vec3 &corner);
+
 	Plane negPlaneX();
 	Plane posPlaneX();
 	Plane negPlaneY();
@@ -23,6 +33,10 @@ public:
 	Intersection * tMaxX(Ray *ray);
 	Intersection * tMaxY(Ray *ray);
 	Intersection * tMaxZ(Ray *ray);
+
+	void tMaxAndTDeltaX(Ray *ray, float &tMaxX, float &tDeltaX);
+	void tMaxAndTDeltaY(Ray *ray, float &tMaxY, float &tDeltaY);
+	void tMaxAndTDeltaZ(Ray *ray, float &tMaxZ, float &tDeltaZ);
 };
 
 #endif
